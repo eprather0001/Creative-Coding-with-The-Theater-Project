098924040cd385a87a9b2319bccda52629f65a5e
eprather0001 <eprather0001@mymail.lausd.net>
2025-04-11 20:49:39 -0700

    Create citations.java

---
+++ citations.java
@@ -0,0 +1,9 @@
+// image citations:
+* https://www.kindpng.com/imgv/TRJJJxi_vector-despicableme-freetoedit-transparent-vector-despicable-me-hd/ | vector image
+* https://despicableme.fandom.com/wiki/Vector's_Fortress | Vector's house (vectorBackground)
+* https://www.seekpng.com/ipng/u2w7q8i1o0i1a9o0_view-full-size-despicable-me-gru-png/ | Gru image
+* https://nohat.cc/f/speech-bubble-left-word-bubble-personalize-it-throw-blanket/m2i8H7m2H7K9b1d3-201908122125.html | left bubble image
+* https://th.bing.com/th/id/OIP.Qz_G_uk7DqOY0h6iirb3sQAAAA?rs=1&pid=ImgDetMain | Vector House
+* https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKeFSwLRzrPJQxadoIByOcTPxTWyJH9AOvNg&s | Science Lab Scene
+* https://pixabay.com/sound-effects/search/explosion/ | Explosion sound effect by Ahmed Abdulaal
+* https://movie-sounds.org/cartoon-samples/quotes-with-sound-clips-from-despicable-me/all-minions-cheering | Minion cheering sound effect 
